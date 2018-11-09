9febf5f3a124a4ae025bace00af4af957f3a6fbb
Adem Pehlivan <adem.phlvn@hotmail.com>
2018-11-09 14:52:54 +0300

    First commit

---
+++ Interfaces/ICharacter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    interface ICharacter
+    {
+        RACE_TYPE Race { get; set; }
+        RACE_CLASS_TYPE RaceClass { get; set; }
+        string CharName { get; set; }
+        RACE_SIDE AlliedRace { get; set; }
+        RACE_SIDE EnemyRaces { get; set; }
+        int Hp { get; set; }
+        int AttackPower { get; set; }
+    }
+
+    
+
+    public enum RACE_TYPE
+    {
+        ELF, ORC,
+        HUMAN, GOBLIN
+    }
+
+    public enum RACE_CLASS_TYPE
+    {
+        WARRIOR, DEATH_KNIGHT,
+        PRIEST, PALADIN
+    }
+
+    public enum RACE_SIDE
+    {
+       HORDE, ALLIANCE
+    }
+
+
+}
---
+++ Interfaces/Program.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+          
+            Random random = new Random();
+          
+         
+
+
+            int ManCount = random.Next(4, 10);
+            for (int i = 0; i < ManCount; i++)
+            {
+              
+            }
+
+           
+
+        
+
+            Console.ReadLine();
+
+        }
+    }
+}
